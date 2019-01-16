d8bb154e95cdd6706c1b45ade1a0605089e08905
Hyyang <475003733@qq.com>
2019-01-16 15:12:35 +0800

    add project hello-python-2

---
+++ hello.py
@@ -0,0 +1,5 @@
+
+# print('hello world')
+print '1024 * 768 =', 1024 * 768
+name = input('please enter your name: ')
+print 'hello,', name
---
+++ my_test1.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+# import selenium print selenium.__file__
+from selenium import webdriver
